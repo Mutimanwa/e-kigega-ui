9266b0c28204e642348970ff0c41da1fafbdd7c6
Mutimanwa <calvinmutimanwa9@gmail.com>
2026-01-06 14:26:21 +0200

    changement de l'architecture

---
+++ config/constantes.php
@@ -0,0 +1,23 @@
+<?php
+// ==========================
+// APPLICATION
+// ==========================
+define('APP_NAME', 'E-Kigega');
+define('APP_VERSION', '1.0.0');
+
+// ==========================
+// PATHS (serveur)
+// ==========================
+define('BASE_PATH', realpath(__DIR__ . '/../') . '/');
+define('CONFIG_PATH', BASE_PATH . 'config/');
+define('INCLUDES_PATH', BASE_PATH . 'includes/');
+define('ASSETS_PATH', BASE_PATH . 'assets/');
+
+// ==========================
+// URL (navigateur)
+// ==========================
+define('BASE_URL', 'http://localhost/e-kigega-ui/');
+define('ASSETS_URL', BASE_URL . 'assets/');
+define('CSS_URL', ASSETS_URL . 'css/');
+define('JS_URL', ASSETS_URL . 'js/');
+define('IMAGES_URL', ASSETS_URL . 'images/');
---
+++ includes/sidebar.php
@@ -0,0 +1,123 @@
+    <!-- leftbar-tab-menu -->
+    <div class="startbar d-print-none">
+        <!--start brand-->
+        <div class="brand">
+            <a href="index.html" class="logo">
+                <!-- <span>
+                    <img src="assets/images/logo-sm.png" alt="logo-small" class="logo-sm" />
+                </span>
+                <span class="">
+                    <img src="assets/images/logo-light.png" alt="logo-large" class="logo-lg logo-light" />
+                    <img src="assets/images/logo-dark.png" alt="logo-large" class="logo-lg logo-dark" />
+                </span> -->
+            </a>
+        </div>
+        <!--end brand-->
+        <!--start startbar-menu-->
+        <div class="startbar-menu">
+            <div class="startbar-collapse" id="startbarCollapse" data-simplebar>
+                <div class="d-flex align-items-start flex-column w-100">
+                    <!-- Navigation -->
+                    <ul class="navbar-nav mb-auto w-100">
+                        <li class="menu-label mt-2">
+                            <span>Tableau de bord</span>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.html">
+                                <i class="iconoir-report-columns menu-icon"></i>
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="payment.html">
+                                <i class="iconoir-box menu-icon"></i>
+                                <span>Gestion de produits</span>
+                            </a>
+                        </li>
+                    
+                        <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="cards.html">
+                               <i class="iconoir-wallet menu-icon"></i>
+                                <span>Gestion des Depenses</span>
+                                <!-- <span class="badge text-bg-pink ms-auto">03</span> -->
+                            </a>
+                        </li>
+                        <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="taxes.html">
+                                <i class="iconoir-user-square menu-icon"></i>
+                                <span>Gestion des clients</span>
+                               
+                            </a>
+                        </li>
+                         <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="cards.html">
+                                <i class="iconoir-shopping-bag menu-icon"></i>
+                                <span>Gestion des Ventes</span>
+                            </a>
+                        </li>
+                        <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="cards.html">
+                               <i class="iconoir-database menu-icon"></i>
+
+                                <span>Gestion de stock</span>
+                              
+                            </a>
+                        </li>
+                        
+                     
+
+                    
+                        <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                              <i class="iconoir-truck menu-icon"></i>
+
+                                <span class="me-2">Fournisseurs </span>
+                                <span class="badge text-bg-blue ms-auto">comming soon</span>
+                            </a>
+                        </li>
+
+                        <li class="menu-label mt-2">
+                            <small class="label-border">
+                                <div class="border_left hidden-xs"></div>
+                                <div class="border_right"></div>
+                            </small>
+                            <span>Systeme</span>
+                        </li>
+                         <!--end nav-item-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="users.html">
+                                <i class="iconoir-user-circle menu-icon"></i>
+
+                                <span>Utilisateurs</span>
+                            </a>
+                        </li>
+                        <!--end nav-item-->
+
+                       
+                    </ul>
+                    <!--end navbar-nav--->
+                    <div class="update-msg text-center">
+                        <div
+                            class="d-flex justify-content-center bg-light align-items-center thumb-xxl update-icon-box rounded-circle mx-auto">
+                            <!-- <i class="iconoir-peace-hand h3 align-self-center mb-0 text-primary"></i> -->
+                            <img src="assets/images/logos/logo.png" alt="" class="" height="130" />
+                        </div>
+                        <h5 class="mt-3">
+                            Today's <span class="text-white">$2450.00</span>
+                        </h5>
+                        <p class="mb-3 text-muted">Today's best Investment for you.</p>
+                        <a href="javascript: void(0);" class="btn text-primary shadow-sm rounded-pill px-3">Upgarde now</a>
+                    </div>
+                </div>
+            </div>
+            <!--end startbar-collapse-->
+        </div>
+        <!--end startbar-menu-->
+    </div>
