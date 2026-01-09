087e92256a5a4a329394e9142470ecf093f1a616
Audry007 <audrywakanda@gmail.com>
2026-01-09 17:33:20 +0200

    wakanda

--- public/profile.php
+++ public/profile.php
@@ -12,11 +12,12 @@ require_once '../includes/sidebar.php';
             <div class="page-title-box d-md-flex justify-content-md-between align-items-center">
                 <h4 class="page-title">Profil Utilisateur</h4>
                 <div class="">
-                    <ol class="breadcrumb mb-0">
-                        <li class="breadcrumb-item"><a href="<?= BASE_URL ?>">Tableau de bord</a></li>
-                        <li class="breadcrumb-item"><a href="<?= BASE_URL ?>public/admin/utilisateurs/">Utilisateurs</a></li>
-                        <li class="breadcrumb-item active">Profil</li>
-                    </ol>
+                   <ol class="breadcrumb mb-0">
+                                <li class="breadcrumb-item"><a href="#">E-Kigega</a></li>
+                                <li class="breadcrumb-item"><a href="#">Admin</a></li>
+                                <!--end nav-item-->
+                                <li class="breadcrumb-item active">Profile</li>
+                            </ol>
                 </div>                                
             </div><!--end page-title-box-->
         </div><!--end col-->
